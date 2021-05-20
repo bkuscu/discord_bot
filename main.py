8e73aaae3dcbdc4afb9eca0b4a4daf1eca397123
bkuscu <bkuscu@techfak.de>
2021-05-21 00:51:20 +0200

    im too tired i dont know what i did exactly, i dont mind. use the shuffle funtion for quick random shuffle PLEASE

--- main.py
+++ main.py
@@ -18,52 +18,54 @@ class MyClient(discord.Client):
         print("Group shuffler bot is active...")
 
     async def on_message(self, message):
-        global groupSize
+        global groupSize, x
         if message.content.startswith("g"):
             groupSize = message.content.split(' ')[1]
-            print("groupsize is: " + str(groupSize))
 
-            await message.channel.send("Group Shuffler Bot: React on this message to enter group selection pool...")
+            if int(groupSize) > 0:
+                print("groupsize is: " + str(groupSize))
+
+                await message.channel.send("Group Shuffler Bot: type: [$in] to enter OR [$out] to delete the entry"
+                                           , delete_after=60)
+            else:
+                await message.channel.send("Please enter a group size >0 ", delete_after=4)
 
 
         if message.content.startswith("in"):
             if not groupList.__contains__(str(message.author)):
                 groupList.append(str(message.author))
+                print("added " + str(message.author))
                 print(groupList)
 
         if message.content.startswith("out"):
             if groupList.__contains__(str(message.author)):
                 groupList.remove(str(message.author))
                 print("removed " + str(message.author))
-                await message.channel.send("Removed " + str(message.author) + " from list")
-                await asyncio.sleep(3)
-                await message.delete()
+                await message.channel.send("Removed " + str(message.author) + " from list" , delete_after=4)
 
 
         if message.content.startswith("end"):
             groupListFinal = groupList.copy()
 
             pair = []
-
+            x = 0
             while groupListFinal.__sizeof__() != 0:
-                #x is the groupID counter
-                for x in range(0, int(((len(groupListFinal) // int(groupSize))))):
-
                     #y is the group size counter
-                    for y in range(0, int(groupSize)):
-                        random = randint(0, len(groupListFinal) -1)
+
+                for y in range(0, int(groupSize)):
+
+                    random = randint(0, len(groupListFinal) -1)
+                    holder = (groupListFinal[random])
+                    pair.append(holder)
+
+                    groupListFinal.remove(holder)
+                    print("PAIR: ", pair)
+                    print("List of members", groupListFinal)
 
 
-
-                        holder = (groupListFinal[random])
-                        pair.append(holder)
-                        groupListFinal.remove(holder)
-
-                        print("PAIR: ", pair)
-                        print("List of members", groupListFinal)
-
-                    await message.channel.send("Group " + str(x) + " : " + str(pair))
-                    pair.clear()
+                await message.channel.send("Group " + str(x) + " : " + str(pair))
+                pair.clear()
+                x = x+1
 
 client = MyClient()
 client.run(TOKEN)
