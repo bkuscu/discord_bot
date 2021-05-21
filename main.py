b1c60da34690543b816dc5c013e6b460899320ae
bkuscu <bkuscu@techfak.de>
2021-05-21 22:46:36 +0200

    fixed some bugs; command messages are deleted automatically; added logs.txt to track the activity for fun; next feature will be maybe a littel game we will see

--- main.py
+++ main.py
@@ -11,30 +11,34 @@ groupList = []
 groupListFinal = []
 groupSize = 0
 grpBool = False
-f = open("logs.txt", "w")
+
 now = datetime.now()
 current_time = now.strftime("[ %d.%m.%y | %H:%M:%S ] ")
 
-f.write("dfadfs")
-
 class MyClient(discord.Client):
 
-
         async def on_ready(self):
+            f = open("logs.txt", "a")
             print("Group shuffler bot is active...")
+            f.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            f.write('\n')
             f.write(str(current_time) + "Bot is online now..")
             f.write('\n')
+            f.close()
 
         async def on_message(self, message):
+            f = open("logs.txt", "a")
             global groupSize, grpBool
             if message.content.startswith("$grp"):
+                await message.delete()
                 grpBool = True
                 groupSize = message.content.split(' ')[1]
 
                 if int(groupSize) > 0:
                     print("--------------------------------------------------------------------------------------")
                     print("Groupsize for each group: " + str(groupSize))
-                    f.write("-------------------------------------------")
+                    f.write("------------------------------------------------------------------------------------")
+                    f.write('\n')
                     f.write(str(current_time) + "Groupsize for each group: " + str(groupSize))
                     f.write('\n')
 
@@ -48,44 +52,55 @@ class MyClient(discord.Client):
 
 
             if message.content.startswith("$in"):
-                if grpBool:
+                await message.delete()
+                if grpBool.__eq__(True):
                     if not groupList.__contains__(str(message.author)):
                         groupList.append(str(message.author))
                         print("Added " + str(message.author) + " to list")
                         print("Current group pool: " + str(groupList))
-                        await message.channel.send("Added: " + str(message.author), delete_after=4)
+                        await message.channel.send("Added: " + str(message.author), delete_after=10)
                         f.write(str(current_time) + "Added:" + str(message.author))
                         f.write('\n')
                 else:
-                    await message.channel.send("Please create a group selection first.", delete_after=4)
+                    await message.channel.send("Please create a group selection first.", delete_after=10)
+                    f.write(str(current_time) + "Please create a group selection first. @" + str(message.author))
+                    f.write('\n')
 
             if message.content.startswith("$out"):
-                if grpBool:
-                    if groupList.__contains__(str(message.author) and grpBool):
+                await message.delete()
+                if grpBool.__eq__(True):
+                    if groupList.__contains__(str(message.author)):
                         groupList.remove(str(message.author))
                         print("Removed " + str(message.author) + "from list")
-                        await message.channel.send("Removed " + str(message.author) + " from list", delete_after=4)
+                        await message.channel.send("Removed " + str(message.author) + " from list", delete_after=10)
                         f.write(str(current_time) + "Removed:" + str(message.author) + " from list")
                         f.write('\n')
                 else:
-                    await message.channel.send("Please create a group selection first.", delete_after=4)
+                    await message.channel.send("Please create a group selection first.", delete_after=10)
 
             if message.content.startswith("$end"):
+                await message.delete()
 
                 groupListFinal = groupList.copy()
                 random.shuffle(groupListFinal)
                 count = 1
-
+                f.write(str(current_time) + "Shuffling...")
+                f.write('\n')
                 while len(groupListFinal) > 0:
                     pair = groupListFinal[0:int(groupSize)]
                     for x in pair:
                         groupListFinal.remove(x)
                     await message.channel.send("Group " + str(count) + ": " + str(pair))
+                    f.write(str(current_time) + "Group " + str(count) + ": " + str(pair))
+                    f.write('\n')
                     pair.clear()
                     count = count+1
                 groupList.clear()
                 grpBool = False
+                f.write("------------------------------------------------------------------------------------")
+                f.write('\n')
                 print("--------------------------------------------------------------------------------------")
+            f.close()
 
 client = MyClient()
 client.run(TOKEN)
