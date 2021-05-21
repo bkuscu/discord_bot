d2acdde4c38af7aebe88a97fd7021440010b0df6
bkuscu <bkuscu@techfak.de>
2021-05-21 22:07:31 +0200

    added logs not working for now

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 import os
 import random
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -9,60 +10,82 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 groupList = []
 groupListFinal = []
 groupSize = 0
+grpBool = False
+f = open("logs.txt", "w")
+now = datetime.now()
+current_time = now.strftime("[ %d.%m.%y | %H:%M:%S ] ")
+
+f.write("dfadfs")
 
 class MyClient(discord.Client):
 
-    async def on_ready(self):
-        print("Group shuffler bot is active...")
 
-    async def on_message(self, message):
-        global groupSize, x
-        if message.content.startswith("$grp"):
-            groupSize = message.content.split(' ')[1]
+        async def on_ready(self):
+            print("Group shuffler bot is active...")
+            f.write(str(current_time) + "Bot is online now..")
+            f.write('\n')
 
-            if int(groupSize) > 0:
-                print("-------------------------------------------")
-                print("Groupsize for each group: " + str(groupSize))
+        async def on_message(self, message):
+            global groupSize, grpBool
+            if message.content.startswith("$grp"):
+                grpBool = True
+                groupSize = message.content.split(' ')[1]
 
-
-                await message.channel.send("Group Shuffler Bot: type: [$in] to enter OR [$out] to delete the entry"
-                                           , delete_after=60)
-            else:
-                await message.channel.send("Please enter a group size >0 ", delete_after=4)
-
-
-        if message.content.startswith("$in"):
-            if not groupList.__contains__(str(message.author)):
-                groupList.append(str(message.author))
-                print("Added " + str(message.author) + " to list")
-                print("Current group pool: " + str(groupList))
-                await message.channel.send("Added: " + str(message.author), delete_after=4)
-
-        if message.content.startswith("$out"):
-            if groupList.__contains__(str(message.author)):
-                groupList.remove(str(message.author))
-                print("Removed " + str(message.author) + "from list")
-                await message.channel.send("Removed " + str(message.author) + " from list", delete_after=4)
+                if int(groupSize) > 0:
+                    print("--------------------------------------------------------------------------------------")
+                    print("Groupsize for each group: " + str(groupSize))
+                    f.write("-------------------------------------------")
+                    f.write(str(current_time) + "Groupsize for each group: " + str(groupSize))
+                    f.write('\n')
 
 
-        if message.content.startswith("$end"):
+                    await message.channel.send("Group Shuffler Bot: type: [$in] to enter OR [$out] to delete the entry"
+                                               , delete_after=10)
+                    await message.channel.send("To end the selection and get the groups type [$end]"
+                                               , delete_after=10)
+                else:
+                    await message.channel.send("Please enter a group size >0 ", delete_after=4)
 
-            groupListFinal = groupList.copy()
-            random.shuffle(groupListFinal)
-            count = 1
 
-            while len(groupListFinal) > 0:
+            if message.content.startswith("$in"):
+                if grpBool:
+                    if not groupList.__contains__(str(message.author)):
+                        groupList.append(str(message.author))
+                        print("Added " + str(message.author) + " to list")
+                        print("Current group pool: " + str(groupList))
+                        await message.channel.send("Added: " + str(message.author), delete_after=4)
+                        f.write(str(current_time) + "Added:" + str(message.author))
+                        f.write('\n')
+                else:
+                    await message.channel.send("Please create a group selection first.", delete_after=4)
 
-                pair = groupListFinal[0:int(groupSize)]
+            if message.content.startswith("$out"):
+                if grpBool:
+                    if groupList.__contains__(str(message.author) and grpBool):
+                        groupList.remove(str(message.author))
+                        print("Removed " + str(message.author) + "from list")
+                        await message.channel.send("Removed " + str(message.author) + " from list", delete_after=4)
+                        f.write(str(current_time) + "Removed:" + str(message.author) + " from list")
+                        f.write('\n')
+                else:
+                    await message.channel.send("Please create a group selection first.", delete_after=4)
 
-                for x in pair:
-                    groupListFinal.remove(x)
+            if message.content.startswith("$end"):
 
-                await message.channel.send("Group " + str(count) + ": " + str(pair))
-                pair.clear()
-                count = count+1
-            groupList.clear()
-            print("-------------------------------------------")
+                groupListFinal = groupList.copy()
+                random.shuffle(groupListFinal)
+                count = 1
+
+                while len(groupListFinal) > 0:
+                    pair = groupListFinal[0:int(groupSize)]
+                    for x in pair:
+                        groupListFinal.remove(x)
+                    await message.channel.send("Group " + str(count) + ": " + str(pair))
+                    pair.clear()
+                    count = count+1
+                groupList.clear()
+                grpBool = False
+                print("--------------------------------------------------------------------------------------")
 
 client = MyClient()
 client.run(TOKEN)
