191627e90593708900145b4b01af3a1b14bfdddb
bkuscu <bkuscu@techfak.de>
2021-05-21 21:14:39 +0200

    group selection finaly works properly; compromised the amount of code and i think this will work faster as well; last thing to do is delete channel messages when users grp/in/out/end

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 import os
-from random import *
+import random
 from dotenv import load_dotenv
-import asyncio
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -10,7 +9,6 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 groupList = []
 groupListFinal = []
 groupSize = 0
-random = 0
 
 class MyClient(discord.Client):
 
@@ -19,11 +17,13 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         global groupSize, x
-        if message.content.startswith("g"):
+        if message.content.startswith("$grp"):
             groupSize = message.content.split(' ')[1]
 
             if int(groupSize) > 0:
-                print("groupsize is: " + str(groupSize))
+                print("-------------------------------------------")
+                print("Groupsize for each group: " + str(groupSize))
+
 
                 await message.channel.send("Group Shuffler Bot: type: [$in] to enter OR [$out] to delete the entry"
                                            , delete_after=60)
@@ -31,41 +31,38 @@ class MyClient(discord.Client):
                 await message.channel.send("Please enter a group size >0 ", delete_after=4)
 
 
-        if message.content.startswith("in"):
+        if message.content.startswith("$in"):
             if not groupList.__contains__(str(message.author)):
                 groupList.append(str(message.author))
-                print("added " + str(message.author))
-                print(groupList)
+                print("Added " + str(message.author) + " to list")
+                print("Current group pool: " + str(groupList))
+                await message.channel.send("Added: " + str(message.author), delete_after=4)
 
-        if message.content.startswith("out"):
+        if message.content.startswith("$out"):
             if groupList.__contains__(str(message.author)):
                 groupList.remove(str(message.author))
-                print("removed " + str(message.author))
-                await message.channel.send("Removed " + str(message.author) + " from list" , delete_after=4)
+                print("Removed " + str(message.author) + "from list")
+                await message.channel.send("Removed " + str(message.author) + " from list", delete_after=4)
 
 
-        if message.content.startswith("end"):
+        if message.content.startswith("$end"):
+
             groupListFinal = groupList.copy()
+            random.shuffle(groupListFinal)
+            count = 1
 
-            pair = []
-            x = 0
-            while groupListFinal.__sizeof__() != 0:
-                    #y is the group size counter
+            while len(groupListFinal) > 0:
 
-                for y in range(0, int(groupSize)):
+                pair = groupListFinal[0:int(groupSize)]
 
-                    random = randint(0, len(groupListFinal) -1)
-                    holder = (groupListFinal[random])
-                    pair.append(holder)
+                for x in pair:
+                    groupListFinal.remove(x)
 
-                    groupListFinal.remove(holder)
-                    print("PAIR: ", pair)
-                    print("List of members", groupListFinal)
-
-
-                await message.channel.send("Group " + str(x) + " : " + str(pair))
+                await message.channel.send("Group " + str(count) + ": " + str(pair), delete_after=30)
                 pair.clear()
-                x = x+1
+                count = count+1
+            groupList.clear()
+            print("-------------------------------------------")
 
 client = MyClient()
 client.run(TOKEN)
